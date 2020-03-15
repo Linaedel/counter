f24b4e2d5ab097b4050442d16e47520f1290fda2
Maksim Eksler <linaedel@yandex.ru>
2020-03-15 21:16:17 +0300

    Redistributed functions betweeen Service and DAO, splitting responsibilities

--- src/main/java/linte/counter/model/CounterRepository.java
+++ src/main/java/linte/counter/model/CounterRepository.java
@@ -2,17 +2,12 @@ package linte.counter.model;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 /**
- * Некий аналог DAO (правда, не реализующий принцип Single Responsibility, поскольку занимается и управлением
- * жизненным циклом самих Counter).
- * Введён для того, чтобы провести границу между Service- и Data-слоями приложения.
- * С практической точки зрения не более необходим в данном приложении, чем сам Counter, может быть заменён использованием
- * мапы в составе сервиса, что, однако, переложит на неё ещё и задачи "хранения" данных, нарушая уже упомянутый принцип.
+ * Некий аналог DAO, обеспечивающий работу с "репозиторием", в котором хранятся значения.
+ * UPD: Перераспределил функционал между мапой и сервисом для разграничения ответственности.
  */
 @Component
 public class CounterRepository {
@@ -22,51 +17,28 @@ public class CounterRepository {
         this.counterMap = new ConcurrentHashMap<>();
     }
 
-    public boolean createCounter(String name) {
-        boolean completedSuccessfully = false;
-        if (!counterMap.containsKey(name)){
-            this.counterMap.put(name, new Counter(name));
-            completedSuccessfully = true;
-        }
-        return completedSuccessfully;
+    public boolean exists(String name){
+        return this.counterMap.containsKey(name);
     }
 
-    public boolean incrementCounter(String name) {
-        boolean completedSuccessfully = false;
-        if (counterMap.containsKey(name)) {
-            this.counterMap.get(name).increment();
-            completedSuccessfully = true;
-        }
-        return completedSuccessfully;
+    public void addCounter(Counter counter){
+        this.counterMap.put(counter.getName(),counter);
     }
 
-    public String getCounterValue(String name) {
-        String counterValue = "";
-        if (counterMap.containsKey(name)) {
-            counterValue = String.valueOf(this.counterMap.get(name).getValue());
-        }
-        return counterValue;
+    public void updateCounter(Counter counter) {
+        this.counterMap.replace(counter.getName(),counter);
     }
 
-    public boolean deleteCounter(String name) {
-        boolean completedSuccessfully = false;
-        if (counterMap.containsKey(name)) {
-            this.counterMap.remove(name);
-            completedSuccessfully = true;
-        }
-        return completedSuccessfully;
+    public Counter getCounter(String name){
+        return this.counterMap.get(name);
     }
 
-    public String getTotalValue() {
-        ConcurrentHashMap<String, Counter> snapshot = new ConcurrentHashMap<>(counterMap);
-        Long totalCount = 0L;
-        for (String name : snapshot.keySet()){
-            totalCount += snapshot.get(name).getValue();
-        }
-        return String.valueOf(totalCount);
+    public void deleteCounter(String name){
+        this.counterMap.remove(name);
     }
 
-    public List<String> getCountersList() {
-        return Collections.list(this.counterMap.keys());
+    public ConcurrentHashMap<String,Counter> getCounters(){
+        return new ConcurrentHashMap<>(this.counterMap);
     }
+
 }
--- src/main/java/linte/counter/service/Impl/CounterServiceImpl.java
+++ src/main/java/linte/counter/service/Impl/CounterServiceImpl.java
@@ -1,11 +1,14 @@
 package linte.counter.service.Impl;
 
+import linte.counter.model.Counter;
 import linte.counter.model.CounterRepository;
 import linte.counter.service.CounterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class CounterServiceImpl implements CounterService {
@@ -18,32 +21,58 @@ public class CounterServiceImpl implements CounterService {
 
     @Override
     public boolean create(String name) {
-        return repository.createCounter(name);
+        boolean completedSuccessfully = false;
+        if (!repository.exists(name)){
+            repository.addCounter(new Counter(name));
+            completedSuccessfully = true;
+        }
+        return completedSuccessfully;
     }
 
     @Override
     public boolean increment(String name) {
-        return repository.incrementCounter(name);
+        boolean completedSuccessfully = false;
+        if(repository.exists(name)){
+            Counter counter = repository.getCounter(name);
+            counter.increment();
+            repository.updateCounter(counter);
+            completedSuccessfully = true;
+        }
+        return completedSuccessfully;
     }
 
     @Override
     public String getValue(String name) {
-        return repository.getCounterValue(name);
+        String counterValue = "";
+        if(repository.exists(name)){
+            counterValue = String.valueOf(repository.getCounter(name).getValue());
+        }
+        return counterValue;
     }
 
     @Override
     public boolean delete(String name) {
-        return repository.deleteCounter(name);
+        boolean completedSuccessfully = false;
+        if(repository.exists(name)){
+            repository.deleteCounter(name);
+            completedSuccessfully = true;
+        }
+        return completedSuccessfully;
     }
 
     @Override
     public String getTotalValue() {
-        return repository.getTotalValue();
+        ConcurrentHashMap<String, Counter> snapshot = repository.getCounters();
+        Long totalCount = 0L;
+        for (String name : snapshot.keySet()){
+            totalCount += snapshot.get(name).getValue();
+        }
+        return String.valueOf(totalCount);
     }
 
     @Override
     public List<String> list() {
-        return repository.getCountersList();
+        ConcurrentHashMap<String, Counter> snapshot = repository.getCounters();
+        return Collections.list(snapshot.keys());
     }
-
 }
